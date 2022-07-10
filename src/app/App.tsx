9f2815d861c244bf481af1ae4a25bea79d9f059c
Ethan Bloom <ebloom19@gmail.com>
2022-07-10 18:27:16 +0800

    further code refactoring

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,7 +4,6 @@ import './App.scss';
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import { SpellsList } from '../components/Main';
 import { Navbar } from '../components/NavBar';
-import { Register, Login } from '../components/Users';
 import FavoriteContainer from 'src/containers/FavoriteContainer';
 import SearchContainer from 'src/containers/SearchContainer';
 
@@ -17,8 +16,6 @@ export const App: React.FC<{}> = () => {
         <Routes>
           <Route path="/" element={<SearchContainer />} />
           <Route path="/favorites" element={<FavoriteContainer/>} />
-          <Route path="register" element={<Register />} />
-          <Route path="login" element={<Login />} />
         </Routes>
       </header>
     </div>
